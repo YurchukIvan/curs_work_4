f274ca07faf9350d60f550eaab385f68dfe2233f
git config --global user.email <raperelano@gmail.com>
2023-09-30 22:42:33 +0300

    commit

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from src.json_keeping import JSONKeeping
+from src.vacancy import Vacancy
+from src.vacancy_handler import VacancyHandler
+from utils import user_interaction
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    # Создаём экземпляр класса для работы с JSON
+    json_saver = JSONKeeping('vacancies.json')
+    # Записываем все полученные вакансии в JSON
+    user_interaction.get_search_query_json_data(json_saver)
+    # Удаляем из JSON вакансии без зарплаты
+    user_interaction.remove_muddy_vacancies(json_saver)
+
+    # Дальше работаем с экземплярами класса Vacancy
+    all_vacancies_list = json_saver.json_to_instances(Vacancy)
+    # Создаём экземпляр класса-обработчика сущностей вакансий
+    vacancies_handler = VacancyHandler(all_vacancies_list)
+
+    # Выводим топ N вакансий
+    top_vacancies_list = user_interaction.show_top_vacancies_by_salary(vacancies_handler, all_vacancies_list)
+    # Предлагаем отфильтровать топ вакансий и записываем результат в файл
+    user_interaction.filter_and_save_vacancies(vacancies_handler, top_vacancies_list)
+
+    # Очищаем файл JSON с вакансиями
+    print('\nВыход из программы.')
+    json_saver.clear_json_with_vacancies()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
